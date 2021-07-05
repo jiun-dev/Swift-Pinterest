436ef8d0a70fcc3ab149edba60204bc8b5d49914
jiun-dev <jiun.dev@gmail.com>
2021-07-05 22:01:10 +0900

    Initial Commit

---
+++ Marvel/MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApp.swift
+//  Marvel
+//
+//  Created by shinjiun on 2021/07/05.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
